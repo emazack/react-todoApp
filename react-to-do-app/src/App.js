89fee9148e975a625c7375e1fd8125a1a23c2c47
emazack <em.zaccaria@gmail.com>
2021-03-16 12:50:32 +0100

    First step

---
+++ react-to-do-app/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React, { useState } from "react";
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          To do List {inputText}
+        </h1>
+      </header>
+      <Form  setInputText={setInputText}/>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
