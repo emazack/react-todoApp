e43e7caccd68d15afe2cc8f687aef7dd947d22fb
emazack <em.zaccaria@gmail.com>
2021-03-17 16:57:43 +0100

    third step

--- react-to-do-app/src/App.js
+++ react-to-do-app/src/App.js
@@ -1,11 +1,51 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  //state
   const [inputText, setInputText] = useState('');
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  // run once
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+  //use effect
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+  //function
+  const filterHandler = (e) => {
+      switch (status) {
+        case 'completed':
+          setFilteredTodos(todos.filter(todo => todo.completed === true))
+          break;
+        
+        case 'uncompleted':
+          setFilteredTodos(todos.filter(todo => todo.completed === false))
+          break;
+      
+        default:
+          setFilteredTodos(todos)
+          break;
+      }
+    };
+    const saveLocalTodos = (e) => {
+      localStorage.setItem('todos', JSON.stringify(todos))
+    }
+    const getLocalTodos = (e) => {
+      if (localStorage.getItem('todos') === null) {
+        localStorage.setItem('todos', JSON.stringify([]))
+      } else {
+        let todoLocal = JSON.parse(localStorage.getItem("todos"));
+        setTodos(todoLocal);
+      }
+    }
+
   return (
     <div className="App">
       <header>
@@ -13,8 +53,14 @@ function App() {
           To do List {inputText}
         </h1>
       </header>
-      <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText}/>
-      <TodoList todos={todos} setTodos={setTodos} />
+      <Form 
+        todos={todos} 
+        setTodos={setTodos} 
+        inputText={inputText} 
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList todos={todos} setTodos={setTodos} filteredTodos={filteredTodos}  />
     </div>
   );
 }
--- react-to-do-app/src/components/Form.js
+++ react-to-do-app/src/components/Form.js
@@ -1,10 +1,10 @@
 import React from 'react';
  
-const Form = ({ inputText, setInputText, setTodos, todos  }) => {
+const Form = ({ inputText, setInputText, setTodos, todos, setStatus  }) => {
     const inputTextHandler = (e) => {
         // console.log(e.target.value);
         setInputText(e.target.value);
-    }
+    };
     const submitTodoHandler = (e) => {
         e.preventDefault();
         setTodos([
@@ -12,7 +12,11 @@ const Form = ({ inputText, setInputText, setTodos, todos  }) => {
             {text: inputText, completed: false, id: Math.random() * 1000 }
         ]);
         setInputText("");
-    }
+    };
+    const statusHandler = (e) => {
+        setStatus(e.target.value);
+    };
+
     return(
         <form >
             <input value={inputText} onChange={inputTextHandler} className="todo-input" type="text"/>
@@ -20,7 +24,7 @@ const Form = ({ inputText, setInputText, setTodos, todos  }) => {
                 <i className="fas fa-plus square"></i>
             </button>
             <div className="select">
-                <select name="todos" className="filter-todo">
+                <select onChange={statusHandler} name="todos" className="filter-todo">
                 <option value="all">All</option>
                 <option value="completed">Completed</option>
                 <option value="uncompleted">Uncompleted</option>
