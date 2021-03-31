26123ff95f1bcaa459615c0de98adcf8676daccd
ezaccaria <emanuele.zaccaria@yourbiz.it>
2021-03-31 18:00:28 +0200

    insert fetch end point

--- react-to-do-app/src/components/Form.js
+++ react-to-do-app/src/components/Form.js
@@ -2,7 +2,6 @@ import React from 'react';
  
 const Form = ({ inputText, setInputText, setTodos, todos, setStatus  }) => {
     const inputTextHandler = (e) => {
-        // console.log(e.target.value);
         setInputText(e.target.value);
     };
     const submitTodoHandler = (e) => {
@@ -16,12 +15,26 @@ const Form = ({ inputText, setInputText, setTodos, todos, setStatus  }) => {
     const statusHandler = (e) => {
         setStatus(e.target.value);
     };
+    const getDataFromEndPoint = (e) => {
+        e.preventDefault();
+        fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
+                console.log(element);
+            });
+        });
+    };
+
 
     return(
         <form >
             <input value={inputText} onChange={inputTextHandler} className="todo-input" type="text"/>
             <button onClick={submitTodoHandler} className="todo-input" type="submit">
                 <i className="fas fa-plus square"></i>
+            </button>
+            <button onClick={getDataFromEndPoint} className="todo-input" type="submit">
+                <i className="fas fa-cloud-download-alt"></i>
             </button>
             <div className="select">
                 <select onChange={statusHandler} name="todos" className="filter-todo">
